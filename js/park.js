ca7ccf3a1047fd5003377e88455f98ac99fe1aa3
Alex2Pena <attn.Alex.P@gmail.com>
2020-01-28 11:05:38 -0800

    11am merge party

--- js/park.js
+++ js/park.js
@@ -1,17 +1,16 @@
 //global declarations//
-var dogTalk = ['Hi', 'How are you', 'Whats your name', 'how old are you?', 'Smell my butt!', 'Do you like to play with toys?', 'You need a haircut', 'Ilike to cuddle']
 
-var rightThink = document.getElementById('rightThink')
-var leftThink = document.getElementById('leftThink')
 
 //Random Number Generator//
 function randomItem(){
     var randomNumber = Math.floor(Math.random() * dogTalk.length)
     return randomNumber;
 }
-
-//For loop to select random array item from var dogTalk//
-for (var i=0; i<dogTalk.length; i++){
-    
+function InteractionCreator(petCardMe, PetCardNew, helloArray,byeArray){
+    this.petCardMe = petCardMe
+    this.PetCardNew = PetCardNew
+    this.helloArray = helloArray
+    this.byeArray = byeArray
 }
 
+var interact1 = new InteractionCreator(petCardMe,PetCardNew, [])
