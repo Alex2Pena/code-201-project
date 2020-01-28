e3b0c4296d370c45ff910a1c83f0b54ecce2e4db
teddydamian <teddy.damian@gmail.com>
2020-01-28 15:09:49 -0800

    finished CSS signup

---
+++ js/signup.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var profileForm = document.getElementById('signUp1');
+
+profileForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  var username = event.target.nameOne.value;
+  var password = event.target.pwordOne.value;
+  var email = event.target.email.value;
+
+  myProfile = new Profile(username, password, email);
+  var stringify = JSON.stringify(myProfile);
+  localStorage.setItem(username, stringify);
+    console.log(myProfile);
+}
