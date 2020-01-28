13ceca2a0d63348c9f5c3305e9af99dcda31cd65
carrington <carringtonbeard@live.com>
2020-01-28 11:06:07 -0800

    worked on login page css html and JS

--- js/login.js
+++ js/login.js
@@ -1,23 +1,28 @@
-var attempt = 3; // Variable to count number of attempts.
-// Below function Executes on click of login button.
-function validate(){
-var username = document.getElementById("username").value;
-var password = document.getElementById("password").value;
-if ( username == "Formget" && password == "formget#123"){
-alert ("Login successfully");
-window.location = "profile.html"; // Redirecting to other page.
-return false;
+// Name and Password from the register-form
+// var name = document.getElementById('nameOne');
+// var pw = document.getElementById('pwordOne');
+
+// storing input from register-form
+// function store() {
+//     localStorage.setItem('nameOne', nameOne.value);
+//     localStorage.setItem('pwordOne', pwordOne.value);
+// }
+
+// check if stored data from register-form is equal to entered data in the   login-form
+function check() {
+
+    // stored data from the register-form
+    var storedName = localStorage.getItem('nameOne');
+    var storedPw = localStorage.getItem('pwordOne');
+
+    // entered data from the login-form
+    var userName = document.getElementById('nameOne');
+    var userPw = document.getElementById('pwordOne');
+
+    // check if stored data from register-form is equal to data from login form
+   if(userName.value == storedName && userPw.value == storedPw) {
+        alert('You are loged in.');
+    }else {
+        alert('ERROR.');
+    }
 }
-else{
-attempt --;// Decrementing by one.
-alert("You have left "+attempt+" attempt;");
-// Disabling fields after 3 attempts.
-if( attempt == 0){
-document.getElementById("username").disabled = true;
-document.getElementById("password").disabled = true;
-document.getElementById("submit").disabled = true;
-return false;
-}
-}
-}
-validate();
