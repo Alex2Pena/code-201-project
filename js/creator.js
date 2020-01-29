2d4c07357efe74e8959ea9f8b9dfaca7f72f98e6
Alex2Pena <attn.Alex.P@gmail.com>
2020-01-28 16:09:38 -0800

    updated creator.js & global.js

--- js/creator.js
+++ js/creator.js
@@ -4,7 +4,6 @@ var petCardForm = document.getElementById('petCardForm');
 petCardForm.addEventListener('submit', handlePetCardFormSubmit);
 
 
-myProfile = JSON.parse(localStorage.getItem('currentUser'));
 
 var PetCard = function (name, image, sex, age, birthday, owner, dateCollected, dateCreated, goodDog, floofiness, energy, snuggles, appetite, bravery){
   this.name = name;
@@ -24,47 +23,45 @@ var PetCard = function (name, image, sex, age, birthday, owner, dateCollected, d
 };
 
 function handlePetCardFormSubmit(event){
-    event.preventDefault();
-    // var inputs = event.target.getElementsByTagName('input');
-    // var name = inputs[0].value;
-    var name = event.target.petName.value;
-    var image = event.target.image.value;
-    var sex = event.target.sex.value;
-    var age = event.target.age.value;
-    var birthday = '';
-    var goodDog = event.target.goodDog.value;
-    var floofiness = event.target.floofiness.value;
-    var energy = event.target.energy.value;
-    var snuggles = event.target.snuggles.value;
-    var appetite = event.target.appetite.value;
-    var bravery = event.target.bravery.value;
-    
-    // Check if the inputted name already exists on a petCard on this profile 
-    var nameMatch = false;
-    console.log(myProfile.petCards.length);
-    for(var i=0; i< myProfile.petCards.length; i++){
-        var newName = name.toLowerCase();
-        var storedName = myProfile.petCards[i].name.toLowerCase();
-        console.log(newName);
-        console.log(storedName);
-        if(newName===storedName) {
-            nameMatch = true;
-            break;
-        }
-    }
+  event.preventDefault();
+  // var inputs = event.target.getElementsByTagName('input');
+  // var name = inputs[0].value;
+  var name = event.target.petName.value;
+  var image = event.target.image.value;
+  var sex = event.target.sex.value;
+  var age = event.target.age.value;
+  var birthday = '';
+  var goodDog = event.target.goodDog.value;
+  var floofiness = event.target.floofiness.value;
+  var energy = event.target.energy.value;
+  var snuggles = event.target.snuggles.value;
+  var appetite = event.target.appetite.value;
+  var bravery = event.target.bravery.value;
 
-    // If a name match is not found, ok to create a new petCard
-    if (nameMatch) {
-        alert('Oh no! Your profile already includes a pet with that name. You can edit the existing pet or delete it and make a new one. Or, you could make a new version of your pet with a different name. Either way is fine :)');
-    } else {
-        var currentDate = new Date();
-        var myPetCard = new PetCard(name,image, sex, age, birthday, myProfile.username, currentDate, currentDate, 
-            goodDog,floofiness,energy,snuggles,appetite,bravery);
-        myProfile.petCards.push(myPetCard);
-        updateProfileDataInStorage();
+  // Check if the inputted name already exists on a petCard on this profile
+  var nameMatch = false;
+  console.log(myProfile.petCards.length);
+  for(var i=0; i< myProfile.petCards.length; i++){
+    var newName = name.toLowerCase();
+    var storedName = myProfile.petCards[i].name.toLowerCase();
+    console.log(newName);
+    console.log(storedName);
+    if(newName===storedName) {
+      nameMatch = true;
+      break;
     }
   }
 
+  // If a name match is not found, ok to create a new petCard
+  if (nameMatch) {
+    alert('Oh no! Your profile already includes a pet with that name. You can edit the existing pet or delete it and make a new one. Or, you could make a new version of your pet with a different name. Either way is fine :)');
+  } else {
+    var currentDate = new Date();
+    var myPetCard = new PetCard(name,image, sex, age, birthday, myProfile.username, currentDate, currentDate,
+      goodDog,floofiness,energy,snuggles,appetite,bravery);
+    myProfile.petCards.push(myPetCard);
+    updateProfileDataInStorage();
+  }
   // If a name match is not found, ok to create a new petCard
   if (nameMatch===false) {
     var currentDate = new Date();
@@ -75,6 +72,8 @@ function handlePetCardFormSubmit(event){
   } else {
     alert('Oh no! Your profile already includes a pet with that name. You can edit the existing pet or delete it and make a new one. Or, you could make a new version of your pet with a different name. Either way is fine :)');
   }
-
-
 }
+
+
+
+
