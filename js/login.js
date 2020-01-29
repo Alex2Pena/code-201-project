bfc4bbaf5ca221ad533394a3a1127885aa2e56ca
Anthony Horton <hortonadtwitter@gmail.com>
2020-01-29 09:34:40 -0800

    Added some changes to login.html and login.js... still a work in progress. Deleted an extra letter in global.js

--- js/login.js
+++ js/login.js
@@ -1,26 +1,42 @@
-
-// check if stored data from register-form is equal to entered data in the   login-form
+var formSelector = document.getElementById('existingProfileCheck');//JAN28 ANTHONY: ADDING EVENT LISTENER TO LOGIN FORM
+formSelector.addEventListener('submit', check);
 function check() {
+    var userName = document.getElementById('nameOne').value;
+    var userPw = document.getElementById('pwordOne').value;
+    if(localStorage.getItem(userName)){//JAN29 ANTHONY: CHECK TO SEE IF USERNAME EXISTS ALREDY... IF IT DOES, THEN CHECK PASSWORD TO COMPLETE LOGIN
+        do{
+            if (localStorage.getItem(userName).password == userPw){
+                populateLocal();//JAN29 ANTHONY: AT THIS POINT LOGIN IS VALIDATED... AND USER GOES TO PROFILE PAGE? OR??
+            } else { //JAN29 ANTHONY: ELSE USERNAME EXISTS, BUT PASSWORD IS INCORRECT. LEAVE USERNAME INPUT POPULATED, CLEAR PASSWORD INPUT FIELD
 
-    // entered data from the login-form
-    var userName = document.getElementById('nameOne');
-    var userPw = document.getElementById('pwordOne');
+            }
+        } while (localStorage.getItem(userName).password !== userPw)//JAN29 ANTHONY: WE SHOULD BE ABLE TO REMAIN IN THIS LOOP SINCE USERNAME EXISTS?
+        
+    } else alert('ERROR.');
 
-    // stored data from the register-form
-    var storedProfile =JSON.parse(localStorage.getItem(userName));
-    var storedPw = storedProfile.password;
-    var storedName = storedProfile.name;
+//JAN28 ANTHONY: COMMENTED OUT BELOW BLOCK THAT CARRINGTON BUILT; HOPING TO RESTRUCTURE 'CHECK' LOGIC UPON FORM SUBMISSION (AND NOT WHILE FORM IS BEING BUILT)
+// check if stored data from register-form is equal to entered data in the   login-form
+// function check() {
+
+//     // entered data from the login-form
+    // var userName = document.getElementById('nameOne');
+    // var userPw = document.getElementById('pwordOne');
+
+//     // stored data from the register-form
+    // var storedProfile =JSON.parse(localStorage.getItem(userName));
+//     var storedPw = storedProfile.password;
+//     var storedName = storedProfile.name;
 
 
-    // check if stored data from register-form is equal to data from login form
-   if(userName.value == storedName && userPw.value == storedPw) {
-        alert('You are loged in.');
-    }else {
-        alert('ERROR.');
-    }
-}
+//     // check if stored data from register-form is equal to data from login form
+//    if(userName.value == storedName && userPw.value == storedPw) {
+//         alert('You are loged in.');
+//     }else {
+//         alert('ERROR.');
+//     }
+// }
 
-check();
+// check();//JAN28 ANTHONY
 
 function populateLocal(){
     var currentUser = JSON.stringify(storedProfile)
